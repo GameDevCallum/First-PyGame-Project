f3e908fbfbce916c19024abe8eae064261ed4893
HyperCodeMarkup <hypercodemarkup@gmail.com>
2020-06-19 12:02:34 +0100

    Added images

--- main.py
+++ main.py
@@ -18,6 +18,14 @@ WIDTH = 500
 WIN = pygame.display.set_mode((HEIGHT, WIDTH))
 pygame.display.set_caption("First Game")
 
+#LOADING ASSETS
+
+walkRight = [pygame.image.load("../R1.png"), pygame.image.load("../R2.png"), pygame.image.load("../R3.png"), pygame.image.load("../R4.png"), pygame.image.load("../R5.png"), pygame.image.load("../R6.png"), pygame.image.load("../R7.png"), pygame.image.load("../R8.png"), pygame.image.load("../R9.png"), ]
+walkLeft = [pygame.image.load("../L1.png"), pygame.image.load("../L2.png"), pygame.image.load("../L3.png"), pygame.image.load("../L4.png"), pygame.image.load("../L5.png"), pygame.image.load("../L6.png"), pygame.image.load("../L7.png"), pygame.image.load("../L8.png"), pygame.image.load("../L9.png"), ]
+bg = pygame.image.load("../bg.jpg")
+char = pygame.image.load("../standing.png")
+
+
 #VARIABLES
 
 plrX = 0
@@ -27,6 +35,9 @@ plrHeight = 40
 plrVel = 5
 isJump = False
 jumpCount = 8
+left = False
+right = True
+walkCount = 0
 
 run = True
 
