63cf5cee0d8856c08f2c9054004bdfd2589ab231
HyperCodeMarkup <hypercodemarkup@gmail.com>
2020-06-19 09:46:03 +0100

    Added Exit funtion and basic setup

--- main.py
+++ main.py
@@ -4,3 +4,37 @@
     DATE FINISHED: {{DD/MM/YYYY}} N/A
     EDITED BY: Callum S
 """
+
+import pygame
+
+pygame.init()
+
+# WIN SETUP
+
+HEIGHT = 500
+WIDTH = 500
+
+WIN = pygame.display.set_mode((HEIGHT, WIDTH))
+pygame.display.set_caption("First Game")
+
+#VARIABLES
+
+x = 50
+y = 50
+width = 40
+height = 60
+vel = 5
+
+run = True
+
+
+while run:
+    pygame.time.delay(100)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            break
+    
+
+pygame.quit()
