5f77bec12eaca9d119bf1b34c4c8c86651399fed
HyperCodeMarkup <hypercodemarkup@gmail.com>
2020-06-20 09:29:52 +0100

    Made it OO

--- main.py
+++ main.py
@@ -30,34 +30,38 @@ char = pygame.image.load(os.path.join("Media", "standing.png"))
 
 clock = pygame.time.Clock()
 
-plrX = 50
-plrY = 400
-plrWidth = 64
-plrHeight = 64
-plrVel = 5
-isJump = False
-jumpCount = 8
-left = False
-right = True
-walkCount = 0
+class player(object):
+    def __init__(self, plrX, plrY, plrWidth, plrHeight):
+        self.plrX = plrX
+        self.plrY = plrY
+        self.plrWidth = plrWidth
+        self.plrHeight = plrHeight
+        self.plrVel = 5
+        self.isJump = False
+        self.jumpCount = 8
+        self.left = False
+        self.right = False
+        self.walkCount = 0
+
+    def draw(self, WIN):
+        if self.walkCount + 1 >= 27:
+            self.walkCount = 0
+        
+        if self.left:
+            WIN.blit(walkLeft[self.walkCount//3], (self.plrX, self.plrY))
+            self.walkCount += 1
+        elif self.right:
+            WIN.blit(walkRight[self.walkCount//3], (self.plrX, self.plrY))
+            self.walkCount += 1
+        else:
+            WIN.blit(char, (self.plrX, self.plrY))
 
 def reDrawGameWindow():
-    global walkCount
     WIN.blit(bg, (0, 0))
-
-    if walkCount + 1 >= 27:
-        walkCount = 0
-    
-    if left:
-        WIN.blit(walkLeft[walkCount//3], (plrX, plrY))
-        walkCount += 1
-    elif right:
-        WIN.blit(walkRight[walkCount//3], (plrX, plrY))
-        walkCount += 1
-    else:
-        WIN.blit(char, (plrX, plrY))
-    
+    character.draw(WIN)
     pygame.display.update()
+
+character = player(300, 410, 64, 64)
 
 run = True
 
@@ -71,37 +75,37 @@ while run:
     
     keys = pygame.key.get_pressed()
     
-    if keys[pygame.K_a] and plrX > plrVel:
-        plrX -= plrVel
-        left = True
-        right = False
+    if keys[pygame.K_a] and character.plrX > character.plrVel:
+        character.plrX -= character.plrVel
+        character.left = True
+        character.right = False
 
-    elif keys[pygame.K_d] and plrX < WIDTH - plrWidth - plrVel:
-        plrX += plrVel
-        right = True
-        left = False
+    elif keys[pygame.K_d] and character.plrX < WIDTH - character.plrWidth - character.plrVel:
+        character.plrX += character.plrVel
+        character.right = True
+        character.left = False
     else:
-        right = False
-        left = False
-        walkCount = 0
+        character.right = False
+        character.left = False
+        character.walkCount = 0
 
-    if not(isJump):
+    if not(character.isJump):
         if keys[pygame.K_w]:
-            isJump = True
-            right = False
-            left = False
-            walkCount = 0
+            character.isJump = True
+            character.right = False
+            character.left = False
+            character.walkCount = 0
     else:
-        if jumpCount >= -8:
+        if character.jumpCount >= -8:
             neg = 1
-            if jumpCount < 0:
+            if character.jumpCount < 0:
                 neg = -1
-            plrY -= (jumpCount ** 2) * 0.5 * neg
-            jumpCount -= 1
+            character.plrY -= (character.jumpCount ** 2) / 1 * neg
+            character.jumpCount -= 1
 
         else:
-            isJump = False
-            jumpCount = 8
+            character.isJump = False
+            character.jumpCount = 8
     
     reDrawGameWindow()
 
